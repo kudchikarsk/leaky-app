e6f578b0d8a44fb451d739ae30309f5f8c40e862
Shadman Kudchikar <shadmanssk@yahoo.in>
2019-07-20 22:02:29 +0530

    Add project files.

---
+++ LeakyApp/Clock.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace LeakyApp
+{
+    /// <summary>
+    /// Interaction logic for Clock.xaml
+    /// </summary>
+    public partial class Clock : Window
+    {
+        DispatcherTimer timer;
+
+        public Clock()
+        {
+            InitializeComponent();
+            timer = new DispatcherTimer
+            {
+                Interval = new TimeSpan(0, 0, 1)
+            };
+
+            timer.Start();
+            timer.Tick += UpdateTime;
+        }
+
+        private void UpdateTime(object sender, EventArgs e)
+        {
+            timerText.Content = DateTime.Now.ToLongTimeString();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            timer.Tick -= UpdateTime;
+            timer.Stop();
+        }
+    }
+}
